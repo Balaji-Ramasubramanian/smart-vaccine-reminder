b2684605ab46c2dc91b18f1fba0b78e57970a219
Balaji <balaji030698@gmail.com>
2018-06-05 14:32:50 +0530

    fix in quick reply text

--- facebookBot/json_templates/quick_replies_strings/quick_replies_ta.rb
+++ facebookBot/json_templates/quick_replies_strings/quick_replies_ta.rb
@@ -3,12 +3,12 @@ class MessengerBot
 QUICK_REPLY_TA = [
 			{
 		    	"content_type": 'text',
-		    	"title": UPCOMING_VACCINATION_MESSAGE_TEXT["en"] ,
+		    	"title": UPCOMING_VACCINATION_MESSAGE_TEXT["ta"] ,
 		    	"payload": "UPCOMING_VACCINATIONS"
 		  	},
 		  	{
 		    	"content_type": 'text',
-		    	"title": PREVIOUS_VACCINATION_MESSAGE_TEXT["en"],
+		    	"title": PREVIOUS_VACCINATION_MESSAGE_TEXT["ta"],
 		    	"payload": "PREVIOUS_VACCINATIONS"
 		  	},
 				
