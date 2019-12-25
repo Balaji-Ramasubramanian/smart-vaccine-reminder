7886f62a6d7fb9552fe613df2063d0f2aba66522
balaji030698 <balaji030698@gmail.com>
2019-12-25 19:04:59 +0530

    Changed the graph api version to 4.0

--- utils.rb
+++ utils.rb
@@ -1,5 +1,11 @@
-FB_PAGE = "https://graph.facebook.com/v2.6/me/messenger_profile?access_token=" + ENV["FB_ACCESS_TOKEN"]
-FB_MESSAGE = "https://graph.facebook.com/v2.6/me/messages?access_token=" + ENV["FB_ACCESS_TOKEN"]
-FB_PROFILE = "https://graph.facebook.com/v2.6/"
+# Facebook page graph API to authenticate to a Facebook Page
+FB_PAGE = "https://graph.facebook.com/v4.0/me/messenger_profile?access_token=" + ENV["FB_ACCESS_TOKEN"]
+# Facebook Message API
+FB_MESSAGE = "https://graph.facebook.com/v4.0/me/messages?access_token=" + ENV["FB_ACCESS_TOKEN"]
+# Facebook graph API URL to get user profile
+FB_PROFILE = "https://graph.facebook.com/v4.0/"
+# Facebook graph API to get profile- fileds to be get.
 FB_PROFILE_FIELDS = "?fields=first_name,last_name,profile_pic,locale,gender&access_token=" + ENV["FB_ACCESS_TOKEN"]
+
+# JSON header content
 HEADER = { "Content-Type" => "application/json" }
